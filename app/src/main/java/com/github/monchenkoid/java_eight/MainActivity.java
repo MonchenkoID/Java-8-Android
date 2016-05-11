226f6b2b0a8b618d05469e95d9fe7ea571a187b1
Iryna Monchanka <iryna_monchanka@epam.com>
2016-05-11 17:15:50 +0300

    Begin of UI

--- app/src/main/java/com/github/monchenkoid/java_eight/MainActivity.java
+++ app/src/main/java/com/github/monchenkoid/java_eight/MainActivity.java
@@ -2,6 +2,8 @@ package com.github.monchenkoid.java_eight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.listFeatures);
+        String[] arrayList = getBaseContext().getResources().getStringArray(R.array.features);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener((adapterView, view, i, l) -> {
+            String strings = arrayList[i];
+            switch (strings){
+                case "":
+            }
+        });
     }
 }
